6d72495b9e9bf46fd36fe1e5406069ee6c2bb0d6
Juanse036 <juansebas036@gmail.com>
2022-06-16 17:04:06 -0500

    Deploy Changes

--- server.js
+++ server.js
@@ -16,14 +16,7 @@ app.use('/clave', require('./routes/api/archivosclave'));
 app.use('/xml', require('./routes/api/archivosxml'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-  //Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  });
-}
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 
 app.listen(port, () => {
