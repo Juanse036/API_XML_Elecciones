bdbd72cf8784a86a67fda83d2e7a55a535a99b91
Juanse036 <juansebas036@gmail.com>
2022-06-16 16:26:35 -0500

    Preprar para Deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.NODE_ENV|| 5000;
+const path = require('path')
 
 
 app.use(express.json());
@@ -10,15 +11,20 @@ app.use(
   })
 );
 
-
 //Definir Rutas
 app.use('/clave', require('./routes/api/archivosclave'));
 app.use('/xml', require('./routes/api/archivosxml'));
 
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  //Set static folder
+  app.use(express.static('client/build'));
 
-app.get('/', (req, res) => {
-  res.json({'message': 'ok'});
-})
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en: http://localhost:${port}`)
