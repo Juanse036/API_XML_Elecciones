7c8f9a136ed62b1e680b1fa5f9e8be49c06688bb
Juanse036 <juansebas036@gmail.com>
2022-06-29 15:21:14 -0500

    Responsive Footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     return (
         <footer className="footer">                                            
             <ul className="social_footer_ul">
-                <li className='footerli'><a href="https://www.linkedin.com/in/juanse036/" className='footera'><img src={linkedin} className="footerimg"/>  Juanse036</a></li>
-                <li className='footerli'><a href="https://github.com/Juanse036" className='footera'><img src={github} className="footerimg" />  Juanse036</a></li>
-                <li className='footerli'><a href="https://twitter.com/juanse036" className='footera'><img src={twitter} className="footerimg" />  Juanse036</a></li>                
+                <li className='footerli'><a href="https://www.linkedin.com/in/juanse036/" className='footera'><img src={linkedin} className="footerimg" alt='LinkedIn'/><div className='footertext'>Juanse036</div></a></li>
+                <li className='footerli'><a href="https://github.com/Juanse036" className='footera'><img src={github} className="footerimg" alt='GitHub'/><div className='footertext'>Juanse036</div></a></li>
+                <li className='footerli'><a href="https://twitter.com/juanse036" className='footera'><img src={twitter} className="footerimg" alt='Twitter'/> <div className='footertext'>Juanse036</div></a></li>      
             </ul>         
     </footer>
     )
