375afb3dc66db070226334d0908eab33616147d0
Juan Sebastián García <juansebas036@gmail.com>
2025-09-16 17:01:26 -0500

    Add Cors policy

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+import cors from "cors";
 const PORT = process.env.PORT || 5000;
 const path = require('path')
 
-
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
